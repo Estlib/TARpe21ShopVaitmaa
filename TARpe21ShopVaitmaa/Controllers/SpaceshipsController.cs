0bad30506f5c0adf7087821d58f1b1c4236d7684
Estlib <75094683+Estlib@users.noreply.github.com>
2023-10-13 10:02:28 +0300

    added spaceship dto and viewmodel

---
+++ TARpe21ShopVaitmaa/Controllers/SpaceshipsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TARpe21ShopVaitmaa.Data;
+
+namespace TARpe21ShopVaitmaa.Controllers
+{
+    public class SpaceshipsController : Controller
+    {
+        private readonly TARpe21ShopVaitmaaContext _context;
+        public SpaceshipsController( TARpe21ShopVaitmaaContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var result = _context.spaceships
+                .OrderBy(x => x.CreatedAt)
+                .Select(x => new SpaceshipIndexViewModel
+                {
+                    ID = x.ID,
+                    Name = x.Name,
+                    Type = x.Type,
+                    PassengerCount = x.PassengerCount,
+                    EnginePower = x.EnginePower,
+                });
+        }
+    }
+}
