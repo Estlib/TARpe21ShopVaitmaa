69726067c8adfdb7bc9cd33201ea17e15c5486af
Estlib <75094683+Estlib@users.noreply.github.com>
2023-10-13 09:40:59 +0300

    migrated database, added dimensions subclass to spaceship model

--- TARpe21ShopVaitmaa.Core/Domain/Spaceship/Spaceship.cs
+++ TARpe21ShopVaitmaa.Core/Domain/Spaceship/Spaceship.cs
@@ -13,7 +13,7 @@ namespace TARpe21ShopVaitmaa.Core.Domain.Spaceship
         public Guid? Id { get; set; } // globally unique identifier
         public string Name { get; set; } // ship name
         public string Description { get; set; } // ship description
-        public int[] Dimensions { get; set; } // contains an array of int, with x y and z values
+        public ICollection<Dimension> Dimensions { get; set; } // contains an pbject of Dimension type, wwhich contains three int values representing width(x) height(y) and depth (z) values
         public int PassengerCount { get; set; } // how many passengers does the ship carry
         public int CrewCount { get; set; } // how many crew members is needed to operate the ship
         public int CargoWeight { get; set; } // how much cargo the ship is able to carry
@@ -34,5 +34,14 @@ namespace TARpe21ShopVaitmaa.Core.Domain.Spaceship
 
         public DateTime CreatedAt { get; set; } // when the entry was created
         public DateTime ModifiedAt { get; set; } // when the entry has been modified last
+    }
+
+    public class Dimension
+    {
+        [Key]
+        public int DimensionID { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int Depth { get; set; }
     }
 }
---
+++ TARpe21ShopVaitmaa.Data/Migrations/20231013063659_spaceship.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TARpe21ShopVaitmaa.Data.Migrations
+{
+    public partial class spaceship : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "spaceships",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PassengerCount = table.Column<int>(type: "int", nullable: false),
+                    CrewCount = table.Column<int>(type: "int", nullable: false),
+                    CargoWeight = table.Column<int>(type: "int", nullable: false),
+                    MaxSpeedInVaccuum = table.Column<int>(type: "int", nullable: false),
+                    BuiltAtDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    MaidenLaunch = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Manufacturer = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsSpaceshipPreviouslyOwned = table.Column<bool>(type: "bit", nullable: false),
+                    FullTripsCount = table.Column<int>(type: "int", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EnginePower = table.Column<int>(type: "int", nullable: false),
+                    FuelConsumptionPerDay = table.Column<int>(type: "int", nullable: false),
+                    MaintenanceCount = table.Column<int>(type: "int", nullable: false),
+                    LastMaintenance = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_spaceships", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Dimension",
+                columns: table => new
+                {
+                    DimensionID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Width = table.Column<int>(type: "int", nullable: false),
+                    Height = table.Column<int>(type: "int", nullable: false),
+                    Depth = table.Column<int>(type: "int", nullable: false),
+                    SpaceshipId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dimension", x => x.DimensionID);
+                    table.ForeignKey(
+                        name: "FK_Dimension_spaceships_SpaceshipId",
+                        column: x => x.SpaceshipId,
+                        principalTable: "spaceships",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Dimension_SpaceshipId",
+                table: "Dimension",
+                column: "SpaceshipId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Dimension");
+
+            migrationBuilder.DropTable(
+                name: "spaceships");
+        }
+    }
+}
